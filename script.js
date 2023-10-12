64ab0dda19659faaeb4984f49f8c6e8ba0f44d39
farrukh <farrukhataev@gmail.com>
2023-10-12 16:15:44 +0300

    java intro

---
+++ script.js
@@ -0,0 +1,52 @@
+   var name = prompt('What`s your name?')
+   console.log("My name is " + name);
+   var age = prompt('how old you are?')
+   console.log("i`m " + age + " years old ");
+   var live = prompt(' where do you live ? ' )
+   console.log('I live in ' + live );
+   var month = prompt('how long have you been coding?')
+   console.log("i`ve been coding for?" + month + " month");
+    //  addition
+   var add = prompt(" exercise-1: 6+3=? ")
+   console.log( "exercise-1: " +  '6+3=' + (6 + 3) + " your answer is 9 ")
+   var add = prompt(" exercise-2: 10+4=? ")
+   console.log(' exercise-2: ' + '10+4=' + (10+4) + " your answer is 14 ")
+   var add = prompt("exercise-3: 20+10=? ")
+   console.log(" exercise-3: " + "20+10=" + (20+10) + " your answer is 30 ");
+    //  subtraction
+   var sub = prompt("exercise-4: 10-5=?")
+   console.log(" exercise-4:"  +  "10-5=" + (10-5) + " your answer is 5 ");
+   var sub = prompt(' exercise-5: 99-9=? ')
+   console.log(" exercise-5: " + "99-9=" + (99-9) + ' your answer is 90 ');
+   var sub = prompt("exercise-6: 25-5=?")
+   console.log("exrcise-6:" + "25-5=" + (25-5) + " your answer is 20 " );
+    //   multiplication
+   var mult = prompt('exercise-7: 7*7=?' )
+   console.log("exercise-7:" + "7*7=" + (7*7) + " your answer is 49");
+   var mult = prompt("exercise-8: 13*13=?")
+   console.log("exercise-8:" + "13*13=" + (13*13) + " your answer is 169 ");
+   var mult = prompt("exercise-9: 5*2+3=?")
+   console.log("exercise-9: " + "5*2+3=" + (5*2+3) + 'your answer is 13');
+   //    division
+   var div = prompt("exercise-10: 12/4=?")
+   console.log("exercise-10:" +  "12/4=" + (12/4) + "your answer is 3");
+   var div = prompt('exercise-11: 121/11=?')
+   console.log("exercise-11:" + "121/11=" + (121/11) + 'your answer is 11');
+   var div = prompt("exercise-12: 36/6=?")
+   console.log("exercise-12:" + "36/6=" + (36/6) + "your answer is 6");
+    // percent/fraction
+    var per = prompt('exercise-13: 13%3=?')
+    console.log("exercise-13:" + "13%3=" + (13%3) + "your answer is 0.1 ");
+    var per = prompt('exercise-14:15%7=?')
+    console.log('exercise-14:' + '15%7=' + (15%7) + 'your answer is 0.1 ');
+    var per = prompt( 'exercise-15: 50%8=?')
+    console.log('exercise-15:' + '50%8=' + (50%8) + "your answer is 0.5")
+    // end
+    var click = prompt('click on the console to get the result  bye :)')
+
+
+ 
+
+
+
+  
